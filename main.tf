a7fc5e16e92b11166fe2a270adab51e7f52cf37d
rcampa01 <rcampa01@estudiantes.unileon.es>
2026-05-06 14:50:18 +0200

    feat: update GitHub Actions workflow and Terraform configuration for AWS credentials

--- main.tf
+++ main.tf
@@ -69,3 +69,7 @@ resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.github_actions_policy.arn
 }
+
+output "role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
